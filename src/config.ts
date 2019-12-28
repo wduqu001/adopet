3b42dff1185edae05559a81883e1a08e9e4f940f
Willian Duque <wduqu001@gmail.com>
2019-12-28 17:36:43 -0300

    adds config.ts file to store api key and urls

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const API_KEY = ''; // Your API_KEY goes here !
+export const API_BASE_URL = 'https://test.adopets.app/v1/';
+
+export const SECTION_ENDPOINT = "auth/session-request";
+export const REGISTER_ENDPOINT = "auth/session-register";
+export const SEARCH_ENDPOINT = "pet/search"; 
