2d69f53382154da734cbefd0c4fe98aabb38ca6b
Willian Duque <wduqu001@gmail.com>
2019-12-29 22:10:30 -0300

    adds test user to config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 export const API_KEY = ''; // Your API_KEY goes here !
 export const API_BASE_URL = 'https://test.adopets.app/v1/';
+export const TEST_USER_EMAIL = 'usuario-test@adopets.com';
+export const TEST_USER_PASSWD = '123123';
 
-export const SECTION_ENDPOINT = "auth/session-request";
-export const REGISTER_ENDPOINT = "auth/session-register";
-export const SEARCH_ENDPOINT = "pet/search"; 
+export const SESSION_REQUEST = "auth/session-request";
+export const SESION_REGISTER = "auth/session-register";
+export const PET_SEARCH = "pet/search"; 
